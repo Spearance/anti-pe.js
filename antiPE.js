97a43b8dd1aecff0a7687cace0e737d87a98bf64
Evgeniy Lepeshkin <spearance@gmail.com>
2021-12-21 01:32:47 +0500

    Заменил регулярное выражение на replaceAll

--- antiPE.js
+++ antiPE.js
@@ -1,17 +1,35 @@
 'use strict';
 
+const mask = {
+	"A": "А",
+	"a": "а",
+	"E": "Е",
+	"e": "е",
+	"T": "Т",
+	"y": "у",
+	"O": "О",
+	"o": "о",
+	"P": "Р",
+	"p": "р",
+	"H": "Н",
+	"K": "К",
+	"X": "Х",
+	"x": "х",
+	"C": "С",
+	"c": "с",
+	"B": "В",
+	"M": "М"
+}
+
 /**
  * @param {string} str
  */
-function antiPE(str){
-	if (str) {
-		var obj = { "A": "А", "a": "а", "E": "Е", "e": "е", "T": "Т", "y": "у", "O": "О", "o": "о", "P": "Р", "p": "р", "H": "Н", "K": "К", "X": "Х", "x": "х", "C": "С", "c": "с", "B": "В", "M": "М" };
-
-		for (var key in obj) {
-			var r = new RegExp(key, 'g');
-			str = str.replace(r, obj[key]);
-		}
+const antiPE = (str) => {
+	if (!str) {
+		return str
 	}
 
-	return str;
+	for (let key in mask) {
+		str = str.replaceAll(key, mask[key])
+	}
 }
